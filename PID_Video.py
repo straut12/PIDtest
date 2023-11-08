90559faf2c65100f36cbeb9a9af86118bf6d29e3
Sean Trautman <trautman_work@hotmail.com>
2023-11-08 13:29:38 -0600

    PID sandbox

--- PID_Video.py
+++ PID_Video.py
@@ -17,21 +17,23 @@ V_i = 0 #initial velocity
 Y_i = 0 #initial height
 #------------
 #---PID GAINS--- 
-#ku = 0.6
-#Tu = 18 ms
-KP = 0.36
-KI = 40.0
-KD = 0.0008099999999999997
+# can use ziegler-nichols to get a good starting point for the gains (heuristic method, practical but not optimal)
+#ku = 0.6  
+#Tu = 18 ms  # distance between peaks of oscillation
+#KP = 0.36  # can start off with 1 for  K gains. Then use ziegler-nichols (Kp=0.6 * ku)
+#KI = 40.0
+#KD = 0.0008099999999999997
 #KD = 0.00128 for higher setpoints
-antiWindup = True
-# KP = 0.6
-# KI = 0.0
-# KD = 0.0
+antiWindup = False
+KP = 0.0
+KI = 0.0
+KD = 0.0
 #---------------
 
-class Simulation(object):
+
+class Simulation(object):  # create a simulation class that inherits object class. Not necessary to pass object class in python 3
 	def __init__(self):
-		self.Insight = Rocket()
+		self.Insight = Rocket() # create a rocket object
 		self.pid = PID(KP,KI,KD,SETPOINT)
 		self.screen = turtle.Screen()
 		self.screen.setup(800,600)
@@ -42,19 +44,19 @@ class Simulation(object):
 		self.marker.color('red')
 		self.sim = True
 		self.timer = 0
-		self.poses = np.array([])
+		self.poses = np.array([]) # store the y positions and times of the rocket
 		self.times = np.array([])
 		self.kpe = np.array([])
 		self.kde = np.array([])
 		self.kie = np.array([])
 		self.thrst = np.array([])
 	def cycle(self):
-		while(self.sim):
-			thrust = self.pid.compute(self.Insight.get_y())
+		while(self.sim):                                     # can set thrust to a constant value for testing
+			thrust = 10 # self.pid.compute(self.Insight.get_y()) # get the thrust value from the PID controller given current y position
 			print(thrust)
-			self.Insight.set_ddy(thrust)
-			self.Insight.set_dy()
-			self.Insight.set_y()
+			self.Insight.set_ddy(thrust) # set the acceleration of the rocket
+			self.Insight.set_dy()        # set the velocity of the rocket
+			self.Insight.set_y()         # set the position of the rocket
 			time.sleep(TIME_STEP)
 			self.timer += 1
 			if self.timer > SIM_TIME:
@@ -72,8 +74,14 @@ class Simulation(object):
 			self.kde = np.append(self.kde,self.pid.get_kde())
 			self.kie = np.append(self.kie,self.pid.get_kie())
 			self.thrst = np.append(self.thrst,thrust)
-		graph(self.times,self.poses,self.kpe,self.kde,self.kie,self.thrst)
+		graph(self.times, self.poses)
+		# graph(self.times,self.poses,self.kpe,self.kde,self.kie,self.thrst)
 
+def graph(x,y):
+	plt.plot(x,y)
+
+
+'''
 def graph(x,y1,y2,y3,y4,y5):
 	fig, (ax1, ax2,ax3,ax4,ax5) = plt.subplots(5, sharex=True)
 	#fig.suptitle('antiwindup')
@@ -88,7 +96,7 @@ def graph(x,y1,y2,y3,y4,y5):
 	ax5.set(ylabel='rocket \nThrust')
 	ax5.plot(x,y5,'tab:brown')
 	plt.show()
-
+'''
 class Rocket(object):
 	def __init__(self):
 		global Rocket
@@ -102,7 +110,7 @@ class Rocket(object):
 		self.ddy = 0
 		self.dy = V_i
 		self.y = INITIAL_Y
-	def set_ddy(self,thrust):
+	def set_ddy(self,thrust): # PID controller will control the thrust
 		self.ddy = g + thrust / MASS
 	def get_ddy(self):
 		return self.ddy
@@ -127,12 +135,13 @@ class PID(object):
 		self.error_last = 0
 		self.derivative_error = 0
 		self.output = 0
-	def compute(self, pos):
+	def compute(self, pos):  # pos is the current position of the rocket
 		self.error = self.setpoint - pos
-		#self.integral_error += self.error * TIME_STEP
-		self.derivative_error = (self.error - self.error_last) / TIME_STEP
+		#self.integral_error += self.error * TIME_STEP  # add up the error over time
+		self.derivative_error = (self.error - self.error_last) / TIME_STEP  # calculate the derivative of the error or how fast it is changing
 		self.error_last = self.error
-		self.output = self.kp*self.error + self.ki*self.integral_error + self.kd*self.derivative_error
+		self.output = self.kp*self.error + self.ki*self.integral_error + self.kd*self.derivative_error # PID formula
+		'''
 		if(abs(self.output)>= MAX_THRUST and (((self.error>=0) and (self.integral_error>=0))or((self.error<0) and (self.integral_error<0)))):
 			if(antiWindup):
 				#no integration
@@ -143,10 +152,11 @@ class PID(object):
 		else:
 			#rectangular integration
 			self.integral_error += self.error * TIME_STEP
-		if self.output >= MAX_THRUST:
+		if self.output >= MAX_THRUST:   # limit the thrust to the max thrust
 			self.output = MAX_THRUST
-		elif self.output <= 0:
+		elif self.output <= 0:          # Don't allow negative thrust
 			self.output = 0
+		'''
 		return self.output
 		
 	def get_kpe(self):
@@ -157,7 +167,7 @@ class PID(object):
 		return self.ki*self.integral_error
 
 def main():
-	sim = Simulation()
-	sim.cycle()
+	sim = Simulation() # create a simulation object
+	sim.cycle()        # call the cycle method to run the simulation
 
 main()
